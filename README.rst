17a1e1667160f194092aea58f136c7207e62ede8
Martin Mahner <martin@mahner.org>
2009-06-17 12:36:07 +0200

    Added license and setup.py.

---
+++ README.rst
@@ -0,0 +1,8 @@
+=============
+django-markup
+=============
+
+This app is a generic way to provide filters that convert text into html.
+
+The documentation is available with this package in the ``docs/`` folder or
+online under this url: http://docs.mahner.org/django-markup/
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-markup',
+    version='0.1',
+    description='A generic Django application to convert text with specific markup to html.',
+    long_description=open('README.rst').read(),
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='http://github.com/bartTC/django-markup/tree/master',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    package_data = {},
+    zip_safe=False,
+)
