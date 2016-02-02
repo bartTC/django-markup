1399861ae977d070bb1ae9a21bae47d3aaf0015f
Martin Mahner <martin@mahner.org>
2016-02-02 08:35:44 +0100

    Removed Wikicreole bundle.
    At the time of writing the initial version WikiCreole was not available as a package, there was no pip and package management was awful.

--- src/django_markup/bundles/WikiCreole/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import doctest
-import creole2html
-
-if __name__ == "__main__":
-    doctest.testmod(creole2html)
--- src/django_markup/filter/creole_filter.py
+++ src/django_markup/filter/creole_filter.py
@@ -1,9 +1,11 @@
 from django_markup.filter import MarkupFilter
 
+
+
 class CreoleMarkupFilter(MarkupFilter):
     title = 'Creole (Wiki Syntax)'
 
     def render(self, text, **kwargs):
-        from django_markup.bundles.WikiCreole.creole import Parser
-        from django_markup.bundles.WikiCreole.creole2html import HtmlEmitter
+        from creole.parser import Parser
+        from creole.html_emitter import HtmlEmitter
         return HtmlEmitter(Parser(text).parse()).emit()
